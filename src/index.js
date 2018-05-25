308271b052fc9eb6ece0d3b0237aabdd315cab8f
hkd987 <lundinmatthews@me.com>
2018-05-25 08:40:45 -0500

    added watchLocation and ClearLocation
    This should allow you to get the updated location when it changes and clear the watcher if needed.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const VueGeolocation = {
   install (Vue) {
     // define the main instance function
     Vue.prototype.$getLocation = VueGeolocation.getLocation
+    Vue.prototype.$watchLocation = VueGeolocation.watchLocation
+    Vue.prototype.$clearLocationWatch = VueGeolocation.clearLocation
   },
   getLocation (options = {}) {
     return new Promise((resolve, reject) => {
@@ -23,6 +25,38 @@ const VueGeolocation = {
       }
     })
   },
+  watchLocation (options = {}) {
+    return new Promise((resolve, reject) => {
+      if (!VueGeolocation._isAvailable()) {
+        reject('no browser support')
+      } else {
+        window.navigator.geolocation.watchPosition(
+          position => {
+            resolve({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            })
+          },
+          () => {
+            reject('no position access')
+          },
+          options
+        )
+      }
+    })
+  },
+  clearLocation (watchID) {
+    return new Promise((resolve, reject) => {
+      if (!VueGeolocation._isAvailable()) {
+        reject('no browser suppor')
+      }
+      else if (!watchID) {
+        reject('please provide watchID')
+      } else {
+        resolve(window.navigator.geolocation.clearWatch(watchID))
+      }
+    })
+  }
   _isAvailable () {
     return 'geolocation' in window.navigator
   }
